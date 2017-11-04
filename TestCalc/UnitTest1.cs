54d5ee2bdcb0208dff190b9b80400456448d8313
Mironova Ekaterina <ekmironova123@gmail.com>
2017-11-04 20:43:30 +0200

    Добавьте файлы проекта.

---
+++ TestCalc/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.Finders;
+
+namespace TestCalc
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow("text_a", true)]
+        [DataRow("text_b", true)]
+        [DataRow("text_op", true)]
+        [DataRow("text_res", true)]
+        public void Calc_Enable(string id, bool exp)
+        {
+            Application application = Application.Launch(@"D:\COURSE\СЕРВЕРА\TEAM\CalcWinForm\CalcWinForm\bin\Debug\CalcWinForm.exe");
+            Window window = application.GetWindows()[0];
+
+            bool res = window.Get<TextBox>(SearchCriteria.ByAutomationId(id)).Enabled;
+            Assert.AreEqual(exp, res);
+            application.Kill();
+        }
+    }
+}
